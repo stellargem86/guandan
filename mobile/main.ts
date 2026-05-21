88c4bd0498b20b414af6dbd141cd517dc309a0bc
stellargem86 <stellargem86@gmail.com>
2026-05-21 15:06:25 +0800

    auto: update files

--- mobile/main.ts
+++ mobile/main.ts
@@ -2,7 +2,10 @@ import { createSSRApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+console.log('main.ts loaded!')
+
 export function createApp() {
+  console.log('createApp called!')
   const app = createSSRApp(App)
   const pinia = createPinia()
   app.use(pinia)
