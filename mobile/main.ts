7f2bd6e4bd9e47348195678bfdbaa573935de10f
stellargem86 <stellargem86@gmail.com>
2026-05-21 15:08:11 +0800

    auto: update files

--- mobile/main.ts
+++ mobile/main.ts
@@ -1,18 +1,17 @@
-import { createSSRApp } from 'vue'
-import { createPinia } from 'pinia'
+import './pages-json-js'
 // @ts-ignore
-import { plugin } from '@dcloudio/uni-h5'
-import './pages.json.js'
+import { plugin as __plugin } from '@dcloudio/uni-h5'
+import { createSSRApp as createVueApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
 console.log('main.ts loaded - manual bootstrap')
 
-function createApp() {
-  const app = createSSRApp(App)
+export function createApp() {
+  const app = createVueApp(App)
   const pinia = createPinia()
   app.use(pinia)
   return { app }
 }
 
-const { app } = createApp()
-app.use(plugin).mount('#app')
+createApp().app.use(__plugin).mount('#app')
