be750af1a4538c333e69927f427a2e4dcd9fb12e
stellargem86 <stellargem86@gmail.com>
2026-05-21 11:29:24 +0800

    auto: update files

---
+++ mobile/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{vue,js,ts}',
+    './components/**/*.{vue,js,ts}',
+    './App.vue',
+    './main.ts',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
